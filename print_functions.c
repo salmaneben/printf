f1c3db7c30b8ee6cab0476f65745ed83391f4ee0
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-25 21:59:03 +0200

    print_function file

---
+++ print_functions.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+void print_string(va_list args)
+{
+	char *s = va_arg(args, char *);  /* get the string to print*/
+	write(1, s, strlen(s));
+}
+
+void print_int(va_list args)
+{
+	int num = va_arg(args, int);
+	char str[50];  /* buffer to hold the string representation of the number*/
+	sprintf(str, "%d", num);  /* convert the number to a string*/
+	write(1, str, strlen(str));
+}
