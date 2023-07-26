a6b1adf93e56b2461371c5cd3584983d968208ab
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-25 17:42:20 -0700

    print_hex file

--- print_hex.c
+++ print_hex.c
@@ -1,12 +1,19 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include "main.h"  
+#include "main.h"
 
 /* Added to have access to my_strlen and va_list */
 
+/**
+ * *convert - function for converting an unsigned integer
+ * @num: represents the unsigned integer value that you want to convert
+ * @base: specifies the base to which you want to convert the number.
+ * Return: Always 0
+ */
+
 char *convert(unsigned int num, int base)
 {
-	static char Representation[]= "0123456789abcdef";
+	static const char Representation[] = "0123456789abcdef";
 	static char buffer[50];
 	char *ptr;
 
@@ -14,16 +21,23 @@ char *convert(unsigned int num, int base)
 	*ptr = '\0';
 
 	do {
-		*--ptr = Representation[num%base];
+		*--ptr = Representation[num % base];
 		num /= base;
-	} while(num != 0);
+	} while (num != 0);
 
-	return(ptr);
+	return (ptr);
 }
 
-void print_hex(va_list args) 
+/**
+ * print_hex - printing an integer as a hexadecimal value
+ * @args: The length argument list
+ * Return: void
+*/
+
+void print_hex(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	char *str = convert(num, 16);
+
 	write(1, str, my_strlen(str));
 }
