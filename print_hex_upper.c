42b340febe8fe7b3698f4ce1b8b1edd7c72e80a7
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-25 18:01:05 -0700

    print_hex_upper file

--- print_hex_upper.c
+++ print_hex_upper.c
@@ -3,9 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * print_hex_upper - printing an integer as a hexadecimal value in uppercase
+ * @args: The length argument list
+ * Return: void
+ */
+
 void print_hex_upper(va_list args)
 {
 	unsigned long num = va_arg(args, unsigned long);
 	char *str = convert_upper(num, 16);
+
 	write(1, str, my_strlen(str));
 }
