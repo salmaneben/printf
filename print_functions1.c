3723aa30d4325f803821827235c7ecb912c1dcb9
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-25 17:52:55 -0700

    print_functions1 file

--- print_functions1.c
+++ print_functions1.c
@@ -3,20 +3,34 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Function to print unsigned integers*/
+/**
+ * print_unsigned - Function to print unsigned integers
+ * @args: The length argument list
+ *
+ * Return: void
+ */
+
 void print_unsigned(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	char str[50];
+
 	sprintf(str, "%u", num);
 	write(1, str, strlen(str));
 }
 
-/* Function to print octal numbers*/
+/**
+ * print_octal - Function to print octal numbers
+ * @args: The length argument list
+ * Return: void
+ */
+
 void print_octal(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
+
 	char str[50];
+
 	sprintf(str, "%o", num);
 	write(1, str, strlen(str));
 }
