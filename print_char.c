1bb7524e56d1603003f1084363379cc8b62f8ff6
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-25 19:48:07 -0700

    update files

--- print_char.c
+++ print_char.c
@@ -1,7 +1,1 @@
-#include "main.h"
 
-void print_char(va_list args)
-{
-char c = va_arg(args, int); /* char is promoted to int when passed through ... */
-write(1, &c, 1);
-}
--- print_hex_upper.c
+++ print_hex_upper.c
@@ -1,18 +1,1 @@
-#include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-/**
- * print_hex_upper - printing an integer as a hexadecimal value in uppercase
- * @args: The length argument list
- * Return: void
- */
-
-void print_hex_upper(va_list args)
-{
-	unsigned long num = va_arg(args, unsigned long);
-	char *str = convert_upper(num, 16);
-
-	write(1, str, my_strlen(str));
-}
